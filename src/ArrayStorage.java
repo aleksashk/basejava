a8a52923842cebd4bbc6c30b4fd774b30ab6468f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-09-12 13:53:08 +0300

    add method findIndex(String uuid)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,12 +1,20 @@
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    protected final Resume[] storage = new Resume[10000];
     private int size = 0;
+
+    public int findIndex(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -18,55 +26,32 @@ public class ArrayStorage {
             System.out.println("Storage is full");
             return;
         }
-        if (resumeChecker(r)) {
-            storage[size++] = r;
-        } else {
+        if (findIndex(r.uuid) != -1) {
             System.out.println("Resume with a uuid '" + r.uuid + "' is already in the storage.");
+            return;
         }
+        storage[size++] = r;
     }
 
     void update(Resume r) {
-        if (resumeChecker(r)) {
+        int index = findIndex(r.uuid);
+        if (index == -1) {
             System.out.println("Resume with a uuid '" + r.uuid + "' isn't in the storage.");
             return;
         }
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(r.uuid, storage[i].uuid)) {
-                storage[i] = r;
-            }
-        }
-    }
-
-    private boolean resumeChecker(Resume r) {
-        return get(r.uuid) == null;
-    }
-
-    private boolean resumeChecker(String uuid) {
-        return get(uuid) == null;
-    }
-
-    Resume get(String uuid) {
-        for (Resume resume : storage) {
-            if (null != resume && Objects.equals(uuid, resume.uuid)) {
-                return resume;
-            }
-        }
-        return null;
+        storage[index] = r;
     }
 
     void delete(String uuid) {
-        if (resumeChecker(uuid)) {
+        int index = findIndex(uuid);
+        if (index == -1) {
             System.out.println("Wrong uuid: " + uuid);
             return;
         }
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(storage[i].uuid, uuid)) {
-                size--;
-                storage[i] = storage[size];
-                storage[size] = null;
-                break;
-            }
-        }
+
+        size--;
+        storage[index] = storage[size];
+        storage[size] = null;
     }
 
     /**
