e398b5e8e69195855a726a8bed202997210f2dd1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-09-26 14:12:01 +0300

    Lesson04 Exceptions

--- src/MainString.java
+++ src/MainString.java
@@ -1,5 +1,3 @@
-package src.ru.javawebinar;
-
 public class MainString {
     public static void main(String[] args) {
         String[] strArray = new String[]{"1","2","3","4","5"};
---
+++ src/src/ru/javawebinar/exception/ExistStorageException.java
@@ -0,0 +1,7 @@
+package src.ru.javawebinar.exception;
+
+public class ExistStorageException extends StorageException {
+    public ExistStorageException(String uuid) {
+        super("Resume " + uuid + " already exist", uuid);
+    }
+}
