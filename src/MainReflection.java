908ed6e162da0ff5c430fc9e811ec8ae4f7faf48
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-09-26 16:30:39 +0300

    toString for resume via reflection

--- src/MainReflection.java
+++ src/MainReflection.java
@@ -1,15 +1,21 @@
 import src.ru.javawebinar.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
+        //TODO: invoke r.toString() via reflection
 
+        Method toString = r.getClass().getDeclaredMethod("toString");
+
+        System.out.println("toString() via reflection: " + toString.invoke(r));
         System.out.println(r);
     }
 }
