ca47a3af7b2ee7fe41eb6ecd48958a27cd42970d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-09-26 15:16:59 +0300

    correct class Resume

--- src/src/ru/javawebinar/model/Resume.java
+++ src/src/ru/javawebinar/model/Resume.java
@@ -1,6 +1,7 @@
 package src.ru.javawebinar.model;
 
 import java.util.Objects;
+import java.util.UUID;
 
 /**
  * Initial resume class
@@ -9,6 +10,14 @@ public class Resume implements Comparable<Resume>{
 
     // Unique identifier
     private String uuid;
+
+    public Resume() {
+        this(UUID.randomUUID().toString());
+    }
+
+    public Resume(String uuid){
+        this.uuid = uuid;
+    }
 
     public String getUuid() {
         return uuid;
