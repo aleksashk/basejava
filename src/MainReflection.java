68a7ae17ea02393cbd11b05fcd25febe4dcecdb4
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-09-26 14:20:45 +0300

    Lesson04 Reflection and jUnit

---
+++ src/MainReflection.java
@@ -0,0 +1,15 @@
+import src.ru.javawebinar.model.Resume;
+
+import java.lang.reflect.Field;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+
+        System.out.println(r);
+    }
+}
